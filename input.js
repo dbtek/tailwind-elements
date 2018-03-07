d383a5e98f3e08b65f60140f66a76b60d0251634
İsmail Demirbilek <ce.demirbilek@gmail.com>
2018-03-08 00:46:48 +0300

    update input classes

--- input.js
+++ input.js
@@ -1,6 +1,6 @@
 var html = require('bel')
 
 module.exports = ({ type = 'text', name = '', placeholder = '', value = '', onChange, required = false, className = ''}) => html `
-  <input class="${className} shadow appearance-none bg-grey-lighter border border-grey-lighter rounded w-full py-3 px-4 text-grey-darker"
+  <input class="${className} shadow appearance-none bg-grey-lighter border border-grey-lighter rounded py-3 px-4 text-grey-darker"
     type=${type} name=${name} placeholder=${placeholder} value=${value} onchange=${onChange} required=${required}>
 `
