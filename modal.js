bd405d3aa05f9ad4661e8e982a5eab3f43aa0188
İsmail Demirbilek <ce.demirbilek@gmail.com>
2018-03-18 13:19:19 +0300

    fix yoify transform

--- modal.js
+++ modal.js
@@ -6,7 +6,7 @@ module.exports = ({ title, content, onClose, actions }) => html `
       <div class="flex py-4">
         <div class="flex-1 font-bold text-xl">${title}</div>
         <button class="w-4 h-4" onclick=${onClose}>
-          <svg class="text-grey hover:text-grey-darkest" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+          <svg class="text-grey hover:text-grey-darkest" role="button" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
         </button>
       </div>
       <div class="pb-4">
