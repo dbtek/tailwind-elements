c1708d268af49604567262338819c3b3202406a7
İsmail Demirbilek <ce.demirbilek@gmail.com>
2018-03-31 12:07:56 +0300

    responsive updates, version bump

--- input-container.js
+++ input-container.js
@@ -2,7 +2,7 @@ var html = require('bel')
 
 module.exports = (label, input) => {
   return html`
-    <div class="px-3 mb-4">
+    <div class="mb-4">
       <label class="block text-grey-darker text-sm font-bold mb-2" for="${input.name}">
         ${label}
       </label>
--- steps.js
+++ steps.js
@@ -3,7 +3,7 @@ var html = require('bel')
 module.exports = (items = []) => html`
   <div class="steps flex items-center justify-center">
     ${items.map(item => html`
-      <div class="step flex-1 flex items-center justify-center ${item.className || ''}" style="vertical-align: -0.38rem;">
+      <div class="step flex-1 items-center justify-center ${item.className || 'flex'}" style="vertical-align: -0.38rem;">
         <span class="flex-1 border-t border-grey"></span>
         <a class="text-white rounded-full py-1 px-2 no-underline mx-4 text-sm flex items-center justify-center shadow-md ${item.active ? 'bg-blue' : 'bg-grey'}" href="${item.href}" onclick=${item.onclick}>${item.label}</a>
         <span class="flex-1 border-t border-grey"></span>
